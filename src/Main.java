1a95f2b12a3ab6aa0e48c167a03eb0e336e60054
derossca <derossca@mail.uc.edu>
2024-02-21 09:49:19 -0500

    made cascaded if structure for playerA move R results

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,7 @@
 //importing scanner
 import java.util.Scanner;
 public class Main {
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         //declaring variables
         Scanner in = new Scanner(System.in);
         String playerA = "";
@@ -12,58 +11,68 @@ public class Main {
         String continueYN = "";
         String trash = "";
 
-        //do while loop for player A move
+        //do while loop that loops the whole game
         do
         {
-           //display the moves
-            System.out.println("Player A " + moves);
-            playerA = in.nextLine(); //getting input then cascading if structure for each potential move or invalid move
+            //do while loop for player A move
+            do
+            {
+                //display the moves
+                System.out.println("Player A " + moves);
+                playerA = in.nextLine(); //getting input then cascading if structure for each potential move or invalid move
+                if (playerA.equalsIgnoreCase("R")) {
+                    done = true;
+                } else if (playerA.equalsIgnoreCase("P")) {
+                    done = true;
+                } else if (playerA.equalsIgnoreCase("S")) {
+                    done = true;
+                } else {
+                    System.out.println("Please enter a valid move: R, P, or S");
+                    System.out.println(); //adding space for easier reading in case of invalid move
+                }
+            } while (!done);
+
+            done = false; // resetting boolean
+
+            //do while loop for playerB move
+            do
+            {
+                //display the moves
+                System.out.println("Player B " + moves);
+                playerB = in.nextLine(); //getting input then cascading if structure for each potential move or invalid move
+                if (playerB.equalsIgnoreCase("R")) {
+                    done = true;
+                } else if (playerB.equalsIgnoreCase("P")) {
+                    done = true;
+                } else if (playerB.equalsIgnoreCase("S")) {
+                    done = true;
+                } else {
+                    System.out.println("Please enter a valid move: R, P, or S");
+                    System.out.println(); //adding space for easier reading in case of invalid move
+                }
+            } while (!done);
+
+            //cascaded if structure to output acceptable results according to the game
             if(playerA.equalsIgnoreCase("R"))
             {
-                done = true;
+                if(playerB.equalsIgnoreCase("R"))
+                {
+                    System.out.println("It's a tie!");
+                }
+                if(playerB.equalsIgnoreCase("P"))
+                {
+                    System.out.println("Paper covers Rock, Player B wins!");
+                }
+                if(playerB.equalsIgnoreCase("S"))
+                {
+                    System.out.println("Rock breaks Scissors, Player A wins!");
+                }
             }
-            else if(playerA.equalsIgnoreCase("P"))
-            {
-                done = true;
-            }
-            else if(playerA.equalsIgnoreCase("S"))
-            {
-                done = true;
-            }
-            else {
-                System.out.println("Please enter a valid move: R, P, or S");
-                System.out.println(); //adding space for easier reading in case of invalid move
-            }
-        } while(!done);
 
-        done = false; // resetting boolean
-
-        //do while loop for playerB move
-        do
-        {
-            //display the moves
-            System.out.println("Player B " + moves);
-            playerB = in.nextLine(); //getting input then cascading if structure for each potential move or invalid move
-            if(playerB.equalsIgnoreCase("R"))
-            {
-                done = true;
-            }
-            else if(playerB.equalsIgnoreCase("P"))
-            {
-                done = true;
-            }
-            else if(playerB.equalsIgnoreCase("S"))
-            {
-                done = true;
-            }
-            else {
-                System.out.println("Please enter a valid move: R, P, or S");
-                System.out.println(); //adding space for easier reading in case of invalid move
-            }
-        } while(!done);
-
-        //do while loop that compares moves and outputs acceptable results with option to continue at the end
-
+            //Prompt to continue
+            System.out.println("Do you want to continue [Y/N] ");
+            continueYN = in.nextLine();
+        } while(continueYN.equalsIgnoreCase("Y"));
     }
 }
 
