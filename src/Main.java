52f4e830b8129a08ff10b90290ae21c03fcf58fb
derossca <derossca@mail.uc.edu>
2024-02-21 09:33:45 -0500

    declared variables, made do while loops and if structures for input variables

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,125 @@
+//importing scanner
+import java.util.Scanner;
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args)
+    {
+        //declaring variables
+        Scanner in = new Scanner(System.in);
+        String playerA = "";
+        String playerB = "";
+        String moves = "enter your move: R, P, or S: ";
+        boolean done = false;
+        String continueYN = "";
+        String trash = "";
+
+        //do while loop for player A move
+        do
+        {
+           //display the moves
+            System.out.println("Player A " + moves);
+            playerA = in.nextLine(); //getting input then cascading if structure for each potential move or invalid move
+            if(playerA.equalsIgnoreCase("R"))
+            {
+                done = true;
+            }
+            else if(playerA.equalsIgnoreCase("P"))
+            {
+                done = true;
+            }
+            else if(playerA.equalsIgnoreCase("S"))
+            {
+                done = true;
+            }
+            else {
+                System.out.println("Please enter a valid move: R, P, or S");
+                System.out.println(); //adding space for easier reading in case of invalid move
+            }
+        } while(!done);
+
+        done = false; // resetting boolean
+
+        //do while loop for playerB move
+        do
+        {
+            //display the moves
+            System.out.println("Player B " + moves);
+            playerB = in.nextLine(); //getting input then cascading if structure for each potential move or invalid move
+            if(playerB.equalsIgnoreCase("R"))
+            {
+                done = true;
+            }
+            else if(playerB.equalsIgnoreCase("P"))
+            {
+                done = true;
+            }
+            else if(playerB.equalsIgnoreCase("S"))
+            {
+                done = true;
+            }
+            else {
+                System.out.println("Please enter a valid move: R, P, or S");
+                System.out.println(); //adding space for easier reading in case of invalid move
+            }
+        } while(!done);
+
+        //do while loop that compares moves and outputs acceptable results with option to continue at the end
+
     }
 }
+
+//do
+//			output “PlayerA choose your move: [R,P,S]”
+//			input moveA
+//			output “PlayerB choose your move: [R,P,S]”
+//			input moveB
+//				if moveA == “R” then
+//					if moveB == “R” then
+//						output “It’s a tie!”
+//					else if move B == “P” then
+//						output “Paper covers Rock, PlayerB wins!”
+//					else
+//						output “Rock breaks Scissors, PlayerA wins!”
+//					end if
+//				else if moveA == “P” then
+//					if moveB == “P” then
+//						output “It’s a tie!”
+//					else if move B == “S” then
+//						output “Scissors cuts Paper, PlayerB wins!”
+//					else
+//						output “Paper covers Rock, PlayerA wins!”
+//					end if
+//				else if moveA == “S” then
+//					if moveB == “S” then
+//						output “It’s a tie!”
+//					else if move B == “R” then
+//						output “Rock breaks Scissors, PlayerB wins!”
+//					else
+//						output “Scissors cuts Paper, PlayerA wins!”
+//					end if
+//
+//				end if
+//			output “Are you done [Y/N]?”
+//			input doneYN
+//		while NOT doneYN == “Y”
+
+
+//if(playerA.equalsIgnoreCase("R"))
+//            {
+//                if(playerB.equalsIgnoreCase("R"))
+//                {
+//                    System.out.println("It's a tie!");
+//                }
+//                else if(playerB.equalsIgnoreCase("P"))
+//                {
+//                    System.out.println("Paper covers Rock, Player B wins!");
+//                }
+//                else if(playerB.equalsIgnoreCase("S"))
+//                {
+//                    System.out.println("Rock smashes Scissors, Player A wins!");
+//                }
+//                else
+//                {
+//                    trash = in.nextLine();
+//                    System.out.println("Please enter R, P, or S, not " + trash);
+//                }
+//            }
